6f727861775cdb8c723f9852ce00fa468b4d89c5
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-02-08 11:44:38 +0200

    cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,24 +2,36 @@
 This module sets up the Flask application, configures CORS for cross-origin 
 requests, and registers the app's blueprint for route handling.
 """
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from app.routes import app_views
 
 cors = CORS()
+
 def create_app():
     """
     Create and configure the Flask application.
     
     This function initializes the Flask app, applies CORS to allow cross-origin 
-    requests from all origins (for development), and registers the app's routes 
+    requests from the specified origin, and registers the app's routes 
     through the 'app_views' blueprint.
     
     Returns:
         app (Flask): The configured Flask application object.
     """
     app = Flask(__name__)
-    cors.init_app(app, resources={r"/*": {"origins": "chatbot-web-five.vercel.app"}})
+
+    cors.init_app(
+        app,
+        resources={
+            r"/*": {
+                "origins": "https://chatbot-web-five.vercel.app/",
+                "methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+                "allow_headers": ["Content-Type"],
+            }
+        },
+    )
+
     app.register_blueprint(app_views)
 
     return app
